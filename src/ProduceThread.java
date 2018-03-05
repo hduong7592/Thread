64ed6b19aec113289d4ca30c850ffa33e34e1e00
hduong7592 <hieuduong08@gmail.com>
2018-03-05 13:39:10 -0500

    Update ProduceThread.java

--- src/ProduceThread.java
+++ src/ProduceThread.java
@@ -3,35 +3,38 @@ import java.security.SecureRandom;
 public class ProduceThread extends Thread {
     private int index;
     private SynchQueue sq;
+    private Object lock;
 
     public ProduceThread(int index, SynchQueue sq) {
         this.index = index;
         this.sq = sq;
+        lock = new Object();
     }
 
     public void run(){
         System.out.println("Producer "+index+" started.");
 
         try{
-            this.sleep(6000);
+            this.sleep(3000);
         }catch(Exception ex){
             System.out.println(ex.getMessage());
         }
+        synchronized (lock) {
+            while (!sq.isDone()) {
 
-        while(!sq.isDone()){
-            int randomNumber = (new SecureRandom()).nextInt(10) + 1;
-            if(sq.enqueue(randomNumber)){
-                //System.out.println(sq);
-                System.out.println("Producer "+index+" added: "+randomNumber+", queue size: "+sq.size());
-                try{
-                    int sleepTime = (new SecureRandom()).nextInt(1000);
-                    this.sleep(sleepTime);
-                }catch(Exception ex){
-                    System.out.println(ex.getMessage());
+                int randomNumber = (new SecureRandom()).nextInt(10) + 1;
+                if (sq.enqueue(randomNumber)) {
+                    //System.out.println(sq);
+                    System.out.println("Producer " + index + " added: " + randomNumber + ", queue size: " + sq.size());
+                    try {
+                        int sleepTime = (new SecureRandom()).nextInt(1000);
+                        this.sleep(sleepTime);
+                    } catch (Exception ex) {
+                        System.out.println(ex.getMessage());
+                    }
+                } else {
+                    sq.shutdown();
                 }
-            }
-            else{
-                sq.shutdown();
             }
         }
     }
