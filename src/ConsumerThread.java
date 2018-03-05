ef4a0608882c72e7cdaf3be9ebde2aa52ce47657
hduong7592 <hieuduong08@gmail.com>
2018-03-05 13:38:30 -0500

    Update ConsumerThread.java

--- src/ConsumerThread.java
+++ src/ConsumerThread.java
@@ -1,48 +1,49 @@
-import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
-
 import java.security.SecureRandom;
 
 public class ConsumerThread extends Thread {
     private int index;
     private SynchQueue sq;
+    private Object lock;
 
     public ConsumerThread(int index, SynchQueue sq) {
         this.index = index;
         this.sq = sq;
+        lock = new Object();
     }
 
     public void run(){
         System.out.println("Consumer "+index+" started.");
 
         try{
-            this.sleep(6000);
+            this.sleep(4000);
         }catch(Exception ex){
             System.out.println(ex.getMessage());
         }
 
-        while (!sq.isEmpty()) {
-            if(!sq.isDone()) {
-                int number;
-                try {
-                    number = (int) sq.dequeue();
-                    System.out.println("Consumer " + index + " removed: " + number + ", computed " + number + "!: " + computeFactorial(number, number - 1));
+        boolean isNotDone = true;
+        synchronized (lock) {
+            while (isNotDone) {
+                int number = (int) sq.dequeue();
+                //System.out.println("Dequeue");
+                if(number >=0 ){
+                    System.out.println("Consumer " + index + " removed: " + number + ", computed " + number + "!: " + computeFactorial(number));
                     try {
                         int sleepTime = (new SecureRandom()).nextInt(1000);
                         this.sleep(sleepTime);
                     } catch (Exception ex) {
                         System.out.println(ex.getMessage());
                     }
-                } catch (Exception ex) {
-                    System.out.println(ex.getMessage());
                 }
+                isNotDone = sq.isDone();
             }
         }
+
     }
 
-    public int computeFactorial(int result, int n){
-        if(n==0) return result;
+    public int computeFactorial(int n){
+        if(n<=1) return 1;
         else{
-            return computeFactorial(result*n, n-1);
+            return n*computeFactorial( n-1);
         }
     }
 }
